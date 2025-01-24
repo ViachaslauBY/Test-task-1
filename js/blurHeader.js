8b6a8c680c24785694f4b3188dc4146400661685
Viachaslau Fedartsou <viachaslaufedartsou@gmail.com>
2025-01-24 18:57:28 +0300

    feat: add sections header and hero

---
+++ js/blurHeader.js
@@ -0,0 +1,6 @@
+let blurHeader = () => {
+  const header = document.getElementById('header');
+  this.scrollY >= 50 ? header.classList.add('blur-header'): header.classList.remove('blur-header');
+}
+
+window.addEventListener('scroll', blurHeader);
