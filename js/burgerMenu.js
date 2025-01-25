f299575174357d812bc894ca1630240695b5810b
Viachaslau Fedartsou <viachaslaufedartsou@gmail.com>
2025-01-25 23:37:11 +0300

    feat: finish task

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,12 +1,16 @@
 const body = document.body;
 const nav = document.querySelector('.header__nav');
 const burgerButton = document.querySelector('.header__burger');
+const header = document.getElementById('header');
 
 if (nav && burgerButton) {
   burgerButton.addEventListener('click', () => {
     nav.classList.toggle('active');    
     burgerButton.classList.toggle('active');    
-    body.classList.toggle('lock'); 
+    body.classList.toggle('lock');
+    if (header.classList.contains('blur-header')) {
+      header.classList.remove('blur-header')
+    }         
     
     nav.querySelectorAll('.nav__link').forEach(link => {
       link.addEventListener('click', () => {
